916ba15e0fe076902bd27a67d163431fbd1e8dc9
Isaac Phiri <isaacphiri315@gmail.com>
2025-02-06 13:18:06 +0200

    feat: update App component to include HowItWorks, Testimonials, Pricing, and Contact components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Hero from './components/Hero';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Testimonials from './components/Testimonials';
+import Pricing from './components/Pricing';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-      <>
-        <Navbar />
-        <Hero />
-        <Features />
-        <Footer />
-      </>
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <Testimonials />
+      <Pricing />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
